490ac724ab3f4af3fd4992fb79d3baf0a236357a
Seow Jian Liang Job <seowjianliang@gmail.com>
2026-06-24 15:11:23 +0800

    Initial Commit

---
+++ ttp-test/ttp_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  ttp_testApp.swift
+//  ttp-test
+//
+//  Created by Jian Liang Job Seow on 24/6/26.
+//
+
+import SwiftUI
+
+@main
+struct ttp_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
